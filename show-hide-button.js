fecddf0ee0515271173e44a617c3fc04cf16d59f
temichk <73367355+temichk@users.noreply.github.com>
2023-07-20 20:30:48 +0400

    Update show-hide-button.js

--- show-hide-button.js
+++ show-hide-button.js
@@ -14,26 +14,12 @@ closeButton.addEventListener('click', function(evt) {
 })
 
 function showAll() {
-
-    
-    // add check if display none, then display block. 
-    // for (var i = 0; i < allServiceButtons.length; i++){
-    //     allServiceButtons[i].style.display = 'none';
-        
-        
-    //     //  if (allServiceButtons[i].style.display === 'none') {
-    //     //     console.log('find')
-    //     //     // allServiceButtons[i].style.display = 'block';
-    //     // }
-    // }
     showButton.style.display = "none";
     closeButton.style.display = "block";
     gallery.style.height = "100%";
 }
 
 function closeAll() {
-    // add check if display block
-    // add function that remember all previous settings of display
     showButton.style.display = "block";
     closeButton.style.display = "none";
     gallery.style.height = "180px";
